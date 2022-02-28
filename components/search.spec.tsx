f8f849eb8de77326a6e581d74eb612379b7974a0
fernandoors <fernando.ors@outlook.com>
2022-02-28 17:20:00 -0300

    feat testing as tdd the component search

---
+++ components/search.spec.tsx
@@ -0,0 +1,35 @@
+import Search from './search';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+const doSearch = jest.fn();
+
+describe('Search', () => {
+  it('should render a form', () => {
+    render(<Search doSearch={doSearch} />);
+    expect(screen.getByRole('form')).toBeInTheDocument();
+  });
+
+  it('should render a input type equals search', () => {
+    render(<Search doSearch={doSearch} />);
+    expect(screen.getByRole('searchbox')).toHaveProperty('type', 'search');
+  });
+
+  it('should call props.doSearch() when form is submitted', async () => {
+    render(<Search doSearch={doSearch} />);
+    const form = screen.getByRole('form');
+    await fireEvent.submit(form);
+    expect(doSearch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call props.doSearch() when the user input', async () => {
+    render(<Search doSearch={doSearch} />);
+    const inputText = 'some text here';
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, inputText);
+    await fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledWith(inputText);
+  });
+});
---
+++ components/search.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+interface SearchProps {
+  doSearch: (term: string) => void;
+}
+export default function Search({ doSearch }: SearchProps): JSX.Element {
+  const [term, setTerm] = useState('');
+  return (
+    <form
+      name="search-form"
+      className="relative mt-6 max-w-lg mx-auto"
+      onSubmit={() => doSearch(term)}
+    >
+      <span className="absolute inset-y-0 left-0 pl-0 pl-3 flex items-center">
+        <svg className="h5 w5 text-gray-500" viewBox="0 0 24 24" fill="none">
+          <path
+            d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </span>
+      <input
+        value={term}
+        onChange={({ target }) => setTerm(target.value)}
+        className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
+        type="search"
+        placeholder="Search"
+      />
+    </form>
+  );
+}
