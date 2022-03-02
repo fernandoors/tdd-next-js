a7f7670542d809ddf494b868238fb36c67f28fc6
fernandoors <fernando.ors@outlook.com>
2022-03-01 23:38:53 -0300

    fix clear default next file _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+/* istanbul ignore file */
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+if (process.env.NODE_ENV === 'development') {
+  require('../miragejs/server').makeServer();
+}
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
