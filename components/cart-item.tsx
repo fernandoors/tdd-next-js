79bfd9e148ce0da54c52cf5c00de087b028ef83d
fernandoors <fernando.ors@outlook.com>
2022-03-01 23:37:08 -0300

    TDD/Component for cart item

---
+++ components/cart-item.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+
+interface CartItemProps {
+  product: Product;
+}
+export default function CartItem({ product }: CartItemProps) {
+  const [quantity, setQuantity] = useState(1);
+
+  const increase = () => setQuantity((prev) => prev + 1);
+
+  const decrease = () => setQuantity((prev) => (!prev ? 0 : prev - 1));
+
+  return (
+    <div data-testid="cart-item" className="flex justify-between mt-6">
+      <div className="flex">
+        <img
+          className="h-20 w-20 object-cover rounded"
+          src={product.image}
+          alt={product.title}
+          data-testid="image"
+        />
+        <div className="mx-3">
+          <h3 className="text-sm text-gray-600">{product.title}</h3>
+          <div className="flex items-center mt-2">
+            <button
+              onClick={decrease}
+              className="text-gray-500 focus:outline-none focus:text-gray-600"
+            >
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+              </svg>
+            </button>
+            <span data-testid="quantity" className="text-gray-700 mx-2">
+              {quantity}
+            </span>
+            <button
+              onClick={increase}
+              className="text-gray-500 focus:outline-none focus:text-gray-600"
+            >
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <span className="text-gray-600">{product.price}$</span>
+    </div>
+  );
+}
---
+++ components/cart-item.unit.spec.tsx
@@ -0,0 +1,63 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import CartItem from './cart-item';
+
+const product = {
+  title: 'Notebook',
+  price: '22.00',
+  image:
+    'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+};
+const renderCartItem = () => {
+  render(<CartItem product={product} />);
+};
+describe('CartItem', () => {
+  it('should render CartItem', () => {
+    renderCartItem();
+    expect(screen.getByTestId('cart-item')).toBeInTheDocument();
+  });
+  it('should display proper content', () => {
+    renderCartItem();
+    const image = screen.getByTestId('image');
+    expect(
+      screen.getByText(new RegExp(product.title, 'i')),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(new RegExp(product.price, 'i')),
+    ).toBeInTheDocument();
+    expect(image).toHaveProperty('src', product.image);
+    expect(image).toHaveProperty('alt', product.title);
+  });
+
+  it('should display 1 as initial quantity', () => {
+    renderCartItem();
+    expect(screen.getByTestId('quantity').textContent).toBe('1');
+  });
+  it('should increase quantity by 1 when second button is clicked', async () => {
+    renderCartItem();
+    const [_, button] = screen.getAllByRole('button');
+    await fireEvent.click(button);
+    expect(screen.getByTestId('quantity').textContent).toBe('2');
+  });
+  it('should decrease quantity by 1 when first button is clicked', async () => {
+    renderCartItem();
+
+    const [decreaseButton, increaseButton] = screen.getAllByRole('button');
+    const quantity = screen.getByTestId('quantity');
+
+    await fireEvent.click(increaseButton);
+    expect(quantity.textContent).toBe('2');
+
+    await fireEvent.click(decreaseButton);
+    expect(quantity.textContent).toBe('1');
+  });
+  it('should not go below zero in the quantity',async()=>{
+    renderCartItem();
+
+    const [decreaseButton] = screen.getAllByRole('button');
+    const quantity = screen.getByTestId('quantity');
+
+    await fireEvent.click(decreaseButton);
+    await fireEvent.click(decreaseButton);
+    expect(quantity.textContent).toBe('0');
+  });
+});
