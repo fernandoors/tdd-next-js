27b35e56a2ec6ce8b60c385e4c1848445b3b0ee5
fernandoors <fernando.ors@outlook.com>
2022-03-04 00:50:47 -0300

    fix product list style view

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
   const { products, error } = useFetchProducts();
   const [term, setTerm] = useState('');
   const [localProducts, setLocalProducts] = useState<Product[]>([]);
-  const addToCart = useCartStore(store=> store.actions.add)
+  const addToCart = useCartStore((store) => store.actions.add);
   useEffect(() => {
     if (!term) {
       setLocalProducts(products);
@@ -25,26 +25,27 @@ const Home: NextPage = () => {
   const productLength = localProducts.length;
   const showProducts = productLength > 0;
   const showEmptyProducts = !showProducts && !error;
+
   return (
     <main data-testid="product-list" className="my-8">
       <Search doSearch={setTerm} />
       <div className="container mx-auto px-6">
-        <h3 className="text-gray-700 text-2x1 font-medium"></h3>
+        <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">
-          {productLength === 1 ? '1 Product' : `${productLength} Products`}
+          {error ? (
+            <h5 data-testid="error-get-products">Erro to load products</h5>
+          ) : null}
         </span>
-        {showProducts &&
-          localProducts.map((product) => (
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+          {localProducts.map((product) => (
             <ProductCard
               key={product.id}
               product={product}
               addToCart={addToCart}
             />
           ))}
-        {showEmptyProducts && <h4 data-testid="no-products">No Products</h4>}
-        {error ? (
-          <h5 data-testid="error-get-products">Erro to load products</h5>
-        ) : null}
+          {showEmptyProducts && <h4 data-testid="no-products">No Products</h4>}
+        </div>
       </div>
     </main>
   );
